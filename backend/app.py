009d15c457cd71de14b1c788fc8c57c6ab2fbd9d
MoryokaV <mariovlaviano2005@yahoo.com>
2022-07-07 21:18:19 +0300

    login system works!

--- backend/app.py
+++ backend/app.py
@@ -1,23 +1,53 @@
 #!/usr/bin/env python3
 
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, session, make_response
+from flask_session import Session
+from functools import wraps
 from pymongo import MongoClient
 from werkzeug.utils import secure_filename
 from bson.objectid import ObjectId
 import os
 import json
+import hashlib
 
 app = Flask(__name__)
 app.config["MEDIA_FOLDER"] = os.path.join(app.root_path, "static/media")
 
-client = MongoClient("");
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
+client = MongoClient(os.getenv("MONGO_URL"));
 db = client.visitbraila
 
+def logged_in(f):
+    @wraps(f)
+    def checkLoginStatus(*args, **kwargs):
+        if session.get("logged_in"):
+            return f(*args, **kwargs)
+        else:
+            return redirect("/login")
+    return checkLoginStatus 
+
+@app.route("/login/", methods=["POST", "GET"])
+def login():
+    if request.method == "POST":
+        print(request.json)
+        if db.login.find_one()["username"] == request.json["user"] and db.login.find_one()["password"] == hashlib.md5(request.json["pass"].encode('utf-8')).hexdigest():
+            session["logged_in"] = True 
+            
+            return make_response("Logged in", 200)
+        else:
+            return make_response("Incorrect details", 401)
+    return render_template("login.html")
+
 @app.route("/admin")
+@logged_in
 def index():
     return render_template("index.html") 
 
 @app.route("/admin/insertSight", methods=["GET", "POST"])
+@logged_in
 def insertSight():
     if request.method == "POST":
         name = request.form["name"] 
