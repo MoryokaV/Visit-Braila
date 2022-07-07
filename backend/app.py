264f755605989ca07378a27b42cfb24d7a5696eb
MoryokaV <mariovlaviano2005@yahoo.com>
2022-07-07 15:12:09 +0300

    implement new db operations

--- backend/app.py
+++ backend/app.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
 
-from flask import Flask, render_template, request, redirect, send_from_directory, flash
+from flask import Flask, render_template, request, redirect
 from pymongo import MongoClient
 from werkzeug.utils import secure_filename
+from bson.objectid import ObjectId
 import os
 import json
 
@@ -17,11 +18,12 @@ def index():
     return render_template("index.html") 
 
 @app.route("/admin/insertSight", methods=["GET", "POST"])
-def secondPage():
+def insertSight():
     if request.method == "POST":
         name = request.form["name"] 
-        
-        #handle images input
+        category = request.form["category"]
+        description = request.form["description"]
+
         paths = []
         for image in request.files.getlist('image'):
             filename = secure_filename(image.filename)
@@ -31,19 +33,35 @@ def secondPage():
             
             image.save(os.path.join(app.config["MEDIA_FOLDER"], path))
         
-        db.sights.insert_one({"name": name, "images": paths})
+        position = request.form["position"]
+
+        db.sights.insert_one({"name": name, "category": category, "description": description, "images": paths, "position": position})
 
         return redirect("/admin")
 
     return render_template("insertSight.html")
 
+@app.route("/admin/listSights")
+def listSights():
+    return render_template("listSights.html")
+
 @app.route("/api/fetchSights")
 def fetchSights():
     return json.dumps(list(db.sights.find()), default=str)
 
-@app.route("/admin/listSights")
-def listSights():
-    return render_template("listSights.html")
+@app.route("/api/deleteSight/<_id>", methods=["DELETE"])
+def deleteSight(_id):
+    db.sights.delete_one({"_id": ObjectId(_id)})
+    return "Successfully deleted document"
+
+@app.route("/api/findSight/<_id>")
+def findSight(_id):
+    return json.dumps(db.sights.find_one({"_id": ObjectId(_id)}), default=str)
+
+@app.route("/api/updateSight/<_id>", methods=["PUT"])
+def updateSight(_id):
+    db.sights.update_one({"_id": ObjectId(_id)}, {"$set": {"name": name, "category": category, "description": description, "images": paths, "position": position}})
+    return "Successfully updated document"
 
 if __name__ == '__main__':
     app.run(debug=True, port=8080);
