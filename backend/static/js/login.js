476bb0fc4c2f4fad48d5dd5cab069dd4fea458fb
MoryokaV <mariovlaviano2005@yahoo.com>
2022-07-14 09:38:21 +0300

    detach js code into separate file

---
+++ backend/static/js/login.js
@@ -0,0 +1,20 @@
+import displayToast from './toast.js';
+
+$(document).ready(function () {
+  $("form button").click(function() {
+    event.preventDefault()
+    $.ajax({
+      url: window.origin + "/login",
+      type: "POST",
+      data: JSON.stringify({"user": $("#user").val(), "pass": $("#pass").val()}),
+      processData: false,
+      contentType: "application/json; charset=UTF-8",
+      success: function(data) {
+        window.location.replace("/admin")
+      },
+      error: function(data) {
+        displayToast(data.responseText, "error")
+      }
+    });
+  });  
+});
--- backend/static/js/toast.js
+++ backend/static/js/toast.js
@@ -1,14 +1,12 @@
-export function displayToast(data, type) {
-    $.toast({
-      heading: "<strong>Success</strong>",
-      text: data,
-      showHideTransition: "fade",
-      position: "top-right",
-      stack: false,
-      icon: type,
-      loaderBg: "rgba(40,167,69,.9)",
-      bgColor: "rgba(40,167,69,.85)",
-    });
+function displayToast(data, type) {
+  $.toast({
+    heading: "<strong>Success</strong>",
+    text: data,
+    showHideTransition: "fade",
+    position: "top-right",
+    stack: false,
+    icon: type,
+  });
 }
 
 export default displayToast;
