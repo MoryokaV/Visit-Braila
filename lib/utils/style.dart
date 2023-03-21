55c8ad2e92375eaa7c3e56a061daa2e49772f8d0
Moryoka <mariovlaviano2005@yahoo.com>
2023-03-21 18:10:36 +0200

    second type of layout for the new cards

--- lib/utils/style.dart
+++ lib/utils/style.dart
@@ -79,3 +79,46 @@ const BoxShadow tinyShadow = BoxShadow(
 
 const String labelFont = "Inter";
 const String bodyFont = "Merriweather";
+
+class TopLeftTriangleClip extends CustomClipper<Path> {
+  @override
+  Path getClip(Size size) {
+    double w = size.width;
+    double h = size.height;
+
+    final path = Path();
+
+    path.moveTo(0, h);
+    path.lineTo(w, h);
+    path.lineTo(w, 0);
+    path.close();
+
+    return path;
+  }
+
+  @override
+  bool shouldReclip(covariant CustomClipper<Path> oldClipper) {
+    return false;
+  }
+}
+
+class BottomRightTriangleClip extends CustomClipper<Path> {
+  @override
+  Path getClip(Size size) {
+    double w = size.width;
+    double h = size.height;
+
+    final path = Path();
+
+    path.lineTo(0, h);
+    path.lineTo(w, 0);
+    path.close();
+
+    return path;
+  }
+
+  @override
+  bool shouldReclip(covariant CustomClipper<Path> oldClipper) {
+    return false;
+  }
+}
