9021e02be86a25c8cbb5cd55d2c120393e511f1d
Moryoka <mariovlaviano2005@yahoo.com>
2023-04-03 12:39:56 +0300

    permit hotels and restaurants in trending

--- backend/static/js/trending.js
+++ backend/static/js/trending.js
@@ -16,21 +16,23 @@ const appendElements = async () => {
 
   $(".trending-container").removeClass("empty").empty();
 
-  const sights = await Promise.all(items.map((item) => getSight(item)));
+  const refItems = await Promise.all(items.map(item => getNameImageItem(item)));
 
   items.map((item, index) => {
+    const { name, image } = refItems[index];
+
     $(".trending-container").append(
       `<article class="trending-item" id="${item._id}">
-        <img src="${sights[index].images[sights[index].primary_image - 1]}" alt="${sights[index].name}">
+        <img src="${image}" alt="${name}">
         <footer>
-          <p>${sights[index].name}</p>
+          <p>${name}</p>
           <div class="loading-spinner"></div>
           <ion-icon name="heart-outline"></ion-icon>
           <button class="btn btn-icon remove-item">
             <ion-icon name="close-outline"></ion-icon>
           </button>
         </footer>
-      </article>`
+      </article> `
     );
   });
 
@@ -66,14 +68,26 @@ const appendElements = async () => {
   });
 }
 
-const getSight = async item => await $.getJSON("/api/findSight/" + item.sight_id);
+const getNameImageItem = async (item) => {
+  if (item.type === "sight") {
+    const { name, images, primary_image } = await $.getJSON("/api/findSight/" + item.item_id);
+    return { "name": name, "image": images[primary_image - 1] };
+  } else if (item.type === "restaurant") {
+    const { name, images, primary_image } = await $.getJSON("/api/findRestaurant/" + item.item_id);
+    return { "name": name, "image": images[primary_image - 1] };
+  } else if (item.type === "hotel") {
+    const { name, images, primary_image } = await $.getJSON("/api/findHotel/" + item.item_id);
+    return { "name": name, "image": images[primary_image - 1] };
+  }
+}
+
 
 $(document).ready(async function() {
   // Initialize
   appendElements();
 
   // Insert item 
-  $("#sight-id").attr("pattern", idRegExp).attr("title", idRegExpTitle);
+  $("#item-id").attr("pattern", idRegExp).attr("title", idRegExpTitle);
 
   $("#add-item").click(function() {
     $(this).addClass("d-none");
@@ -97,20 +111,33 @@ $(document).ready(async function() {
     e.preventDefault();
 
     const item = {
-      sight_id: $("#sight-id").val(),
+      item_id: $("#item-id").val(),
+      type: "",
       index: items.length,
     }
 
-    if (items.filter((i) => i.sight_id === item.sight_id).length > 0) {
+    if (items.filter((i) => i.item_id === item.item_id).length > 0) {
       alert("Item already present in list");
-      $("#sight-id").val("");
+      $("#item-id").val("");
 
       return;
     }
 
-    const sight = await $.getJSON("/api/findSight/" + item.sight_id).catch((error) => alert(error.responseText));
+    const sight = await $.getJSON("/api/findSight/" + item.item_id).catch(() => { });
+    const restaurant = await $.getJSON("/api/findRestaurant/" + item.item_id).catch(() => { });
+    const hotel = await $.getJSON("/api/findHotel/" + item.item_id).catch(() => { });
 
     if (sight !== undefined) {
+      item.type = "sight";
+    } else if (restaurant !== undefined) {
+      item.type = "restaurant";
+    } else if (hotel !== undefined) {
+      item.type = "hotel";
+    } else {
+      alert("ERROR: Not a valid sight/restaurant/hotel id!");
+    }
+
+    if (sight !== undefined || restaurant !== undefined || hotel !== undefined) {
       await $.ajax({
         type: "POST",
         url: "/api/insertTrendingItem",
@@ -123,7 +150,7 @@ $(document).ready(async function() {
     }
 
     $("#add-item").removeClass("d-none");
-    $("#sight-id").val("");
+    $("#item-id").val("");
     $(this).addClass("d-none");
   });
 
