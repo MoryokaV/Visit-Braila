bfba63bd1a586ad93192519fae5c17ad52d91052
Moryoka <mariovlaviano2005@yahoo.com>
2023-10-29 10:13:26 +0200

    fix status bar theme in no_internet_view

--- lib/views/nointernet_view.dart
+++ lib/views/nointernet_view.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:provider/provider.dart';
 import 'package:visit_braila/services/connection_service.dart';
@@ -10,74 +11,77 @@ class NoInternetView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return WillPopScope(
-      onWillPop: () async => false,
-      child: Scaffold(
-        body: SafeArea(
-          child: Padding(
-            padding: const EdgeInsets.symmetric(
-              vertical: 30,
-              horizontal: 15,
-            ),
-            child: Stack(
-              children: [
-                Center(
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    crossAxisAlignment: CrossAxisAlignment.stretch,
-                    children: [
-                      SvgPicture.asset(
-                        "assets/icons/wifi-off.svg",
-                        height: Responsive.safeBlockVertical * 15,
-                        fit: BoxFit.contain,
-                        colorFilter: ColorFilter.mode(kDisabledIconColor, BlendMode.srcIn),
-                      ),
-                      const SizedBox(
-                        height: 30,
-                      ),
-                      Text(
-                        "Oops!",
-                        style: Theme.of(context).textTheme.headlineMedium!.copyWith(
-                              fontSize: 32,
-                              fontWeight: FontWeight.w700,
-                            ),
-                        textAlign: TextAlign.center,
-                      ),
-                      const SizedBox(
-                        height: 15,
-                      ),
-                      Text(
-                        "Conexiunea la internet se pare că este offline. Mai încearcă.",
-                        style: Theme.of(context).textTheme.bodyLarge!.copyWith(
-                              color: kDimmedForegroundColor,
-                            ),
-                        textAlign: TextAlign.center,
-                      ),
-                    ],
-                  ),
-                ),
-                Align(
-                  alignment: Alignment.bottomCenter,
-                  child: Consumer<ConnectionService>(
-                    builder: (context, connection, _) {
-                      return TextButton(
-                        onPressed: connection.isOnline
-                            ? () {
-                                connection.popup = false;
-                                Navigator.of(context).pushReplacementNamed('/');
-                              }
-                            : null,
-                        child: const Text(
-                          "Reîncearcă",
-                          style: TextStyle(
-                            fontSize: 18,
-                          ),
+    return AnnotatedRegion<SystemUiOverlayStyle>(
+      value: SystemUiOverlayStyle.dark,
+      child: WillPopScope(
+        onWillPop: () async => false,
+        child: Scaffold(
+          body: SafeArea(
+            child: Padding(
+              padding: const EdgeInsets.symmetric(
+                vertical: 30,
+                horizontal: 15,
+              ),
+              child: Stack(
+                children: [
+                  Center(
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.stretch,
+                      children: [
+                        SvgPicture.asset(
+                          "assets/icons/wifi-off.svg",
+                          height: Responsive.safeBlockVertical * 15,
+                          fit: BoxFit.contain,
+                          colorFilter: ColorFilter.mode(kDisabledIconColor, BlendMode.srcIn),
                         ),
-                      );
-                    },
+                        const SizedBox(
+                          height: 30,
+                        ),
+                        Text(
+                          "Oops!",
+                          style: Theme.of(context).textTheme.headlineMedium!.copyWith(
+                                fontSize: 32,
+                                fontWeight: FontWeight.w700,
+                              ),
+                          textAlign: TextAlign.center,
+                        ),
+                        const SizedBox(
+                          height: 15,
+                        ),
+                        Text(
+                          "Conexiunea la internet se pare că este offline. Mai încearcă.",
+                          style: Theme.of(context).textTheme.bodyLarge!.copyWith(
+                                color: kDimmedForegroundColor,
+                              ),
+                          textAlign: TextAlign.center,
+                        ),
+                      ],
+                    ),
                   ),
-                ),
-              ],
+                  Align(
+                    alignment: Alignment.bottomCenter,
+                    child: Consumer<ConnectionService>(
+                      builder: (context, connection, _) {
+                        return TextButton(
+                          onPressed: connection.isOnline
+                              ? () {
+                                  connection.popup = false;
+                                  Navigator.of(context).pushReplacementNamed('/');
+                                }
+                              : null,
+                          child: const Text(
+                            "Reîncearcă",
+                            style: TextStyle(
+                              fontSize: 18,
+                            ),
+                          ),
+                        );
+                      },
+                    ),
+                  ),
+                ],
+              ),
             ),
           ),
         ),
