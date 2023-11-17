65556238383b626c7f170056734cd78224973d81
Moryoka <mariovlaviano2005@yahoo.com>
2023-11-17 21:32:22 +0200

    test a new header image

--- lib/views/home_view.dart
+++ lib/views/home_view.dart
@@ -64,7 +64,7 @@ class HomeView extends StatelessWidget {
                               alignment: Alignment.topLeft,
                               children: [
                                 CachedAssetImage(
-                                  "assets/images/braila_night.jpg",
+                                  "assets/images/fantana.jpg",
                                   cacheHeight: Responsive.safeBlockVertical * 35,
                                   cacheWidth: Responsive.screenWidth,
                                 ),
