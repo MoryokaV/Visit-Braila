c0077c09ab1d824c6c20688bfa15a0fb11e43172
Moryoka <mariovlaviano2005@yahoo.com>
2025-04-21 21:29:30 +0300

    Fix path for pdf delete

--- api/src/utils/pdf.ts
+++ api/src/utils/pdf.ts
@@ -1,13 +1,14 @@
 import { Request, Response } from "express";
 import fs from "fs";
+import path from "path";
 
 export const uploadPDF = async (req: Request, res: Response) => {
   res.status(200).send("PDF uploaded successfully");
 };
 
-export const deletePDF = (path: string): void => {
+export const deletePDF = (filepath: string): void => {
   try {
-    fs.unlinkSync("." + path);
+    fs.unlinkSync(path.join(__dirname, "..", "..", filepath));
   } catch (_) {
     //pass
   }
